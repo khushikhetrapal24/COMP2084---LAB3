c60f7eb86166e4762df5c1880b89b3023661faf6
Khushi Khetrapal <200431715@student.georgianc.on.ca>
2020-01-23 20:54:11 -0800

    completed lab3

---
+++ PhoneApplication/Controllers/PhoneController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PhoneApplication.Models;
+namespace PhoneApplication.Controllers
+{
+    public class PhoneController : Controller
+    {
+        // GET: Phone
+        public ActionResult Index()
+        {
+
+            var phonelist = new List<Phone>
+                {
+                    new Phone {PhoneId = 01, PhoneName = "Apple", Manufacturer = "Apple"},
+                new Phone { PhoneId = 02,PhoneName = "Samsung", Manufacturer= "Samsung"},
+
+                };
+            
+            return View(phonelist);
+        }
+    }
+}
